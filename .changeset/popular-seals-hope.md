a1450595277e6a156d9777717e500adf531e963c
Grant Forrest <gaforres@gmail.com>
2024-02-19 20:25:53 -0500

    export missing tools

---
+++ .changeset/popular-seals-hope.md
@@ -0,0 +1,5 @@
+---
+"@a-type/auth": patch
+---
+
+Export missing tools
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from './db.js';
 export * from './handlers.js';
 export * from './providers/index.js';
 export * from './session.js';
+export * from './email.js';
+export * from './error.js';
